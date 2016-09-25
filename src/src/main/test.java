4716df063a979fddba5103d0352a9e6e1cc4242a
chenhe356 <13121988040@163.com>
2016-09-25 20:21:16 +0800

    测试test添加到Github

---
+++ src/src/main/test.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by zhangyan on 16/9/25.
+ */
+public class test {
+    public static void main(String [] args){
+        System.out.println("this is a test main");
+    }
+}
